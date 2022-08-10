eebda700fb0a891a45e93edf99389746db9bf794
DoQuocBinh <binhdq@fpt.edu.vn>
2022-08-10 17:13:57 +0700

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using ConsoleApp8;
+using System.Globalization;
+
+//Console.WriteLine("Enter your name:");
+//String name = Console.ReadLine();
+//Console.WriteLine($"Your name is: {name}");
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var n = DateTime.Now;
+
+CultureInfo vn = new CultureInfo("vi-VN");
+Thread.CurrentThread.CurrentCulture = vn;
+//Console.WriteLine(n.ToLongDateString());
+//Console.WriteLine(vn.DateTimeFormat.GetMonthName(n.Month));
+Console.WriteLine(n.Month + " " + n.Year);
+double amount = 334432.4456;
+Console.WriteLine(String.Format(vn,"{0:c}",amount));
+Console.WriteLine(String.Format(vn, "{0:f}", amount));
+
+
+List<GRWStudent> list = new List<GRWStudent>();
+list.Add(new GRWStudent{ Name="Trang"});
+list.Add(new GRWStudent { Name = "Phuong" });
+Console.WriteLine($"So phan tu la: {list.Count}");
+
